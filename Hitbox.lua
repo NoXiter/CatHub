8d9c616ed8f2d3fa7cb114801e9bffb8321944b2
NoXiter <pedrohenriquee726@gmail.com>
2024-08-03 12:12:08 -0300

    Hitbox.lua

--- Hitbox.lua
+++ Hitbox.lua
@@ -1,6 +1,12 @@
 _G.HeadSize = 8
 _G.Disabled = true
- 
+
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+Title = "CatHub Fly";
+Text = "tbhnoxiter#0000";
+Icon = "rbxthumb://type=Asset&id=5107182114&w=150&h=150"})
+Duration = 6; 
+
 game:GetService('RunService').RenderStepped:connect(function()
 if _G.Disabled then
 for i,v in next, game:GetService('Players'):GetPlayers() do
